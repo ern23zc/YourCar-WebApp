17dfe2c083d59eebd29dc432909f6fdaa14b64ac
ChamorroSamuel <102174595+ChamorroSamuel@users.noreply.github.com>
2023-09-29 21:36:43 -0500

    Update Login
    Update Component Login

--- src/app/public/pages/register-user/register-owner/register-owner.component.ts
+++ src/app/public/pages/register-user/register-owner/register-owner.component.ts
@@ -10,6 +10,31 @@ export class RegisterOwnerComponent {
   lastname='';
   birthdate='';
   phone='';
-  email='';
-  password='';
+  email: string = '';
+  password: string = '';
+  isEmailValid: boolean = true;
+  isPasswordValid: boolean = true;
+  isPasswordNotEmpty: boolean = true;
+  isEmailNotEmpty: boolean = true;
+  isLoginFormValid: boolean = false; // Propiedad para rastrear si el formulario es válido
+
+  // Función de validación personalizada para el correo electrónico
+  validateEmail() {
+    this.isEmailValid = /\b@gmail\.com$/.test(this.email);
+    this.isEmailNotEmpty = this.email.trim() !== '';
+    this.updateFormValidity();
+  }
+
+  // Función de validación personalizada para la contraseña
+  validatePassword() {
+    this.isPasswordValid = this.password.length >= 6;
+    this.isPasswordNotEmpty = this.password.trim() !== '';
+    this.updateFormValidity();
+  }
+
+  // Función para actualizar la validez del formulario
+  updateFormValidity() {
+    this.isLoginFormValid = this.isEmailValid && this.isPasswordValid && this.isEmailNotEmpty && this.isPasswordNotEmpty;
+  }
+
 }
